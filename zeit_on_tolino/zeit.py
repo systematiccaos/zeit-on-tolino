14598fceb84a121b3f0e9feb8324ea6fde70df10
systematiccaos <77922833+systematiccaos@users.noreply.github.com>
2025-08-03 09:18:31 +0200

    Update zeit.py

--- zeit_on_tolino/zeit.py
+++ zeit_on_tolino/zeit.py
@@ -299,7 +299,7 @@ def download_e_paper(webdriver: WebDriver) -> str:
     if BUTTON_TEXT_EPUB_DOWNLOAD_IS_PENDING in webdriver.page_source:
         raise RuntimeError("New ZEIT release is available, however, EPUB version is not. Retry again later.")
     log.info('looking for download button')
-    time.sleep(Delay.small)
+    time.sleep(30)
     for link in webdriver.find_elements(By.TAG_NAME, "a"):
         if link.text == BUTTON_TEXT_DOWNLOAD_EPUB:
             log.info("clicking download button now...")
